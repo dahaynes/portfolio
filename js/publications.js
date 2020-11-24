55e025ac1d6de23dcc1b6ef8207907bd6f1d3c7c
dahaynes <dahaynes@umn.edu>
2020-11-24 12:03:57 -0600

    Update publications.js

--- js/publications.js
+++ js/publications.js
@@ -14,7 +14,7 @@ function loadMetaData(jsonArray) {
 
         
         /* This is a template string a mixture of JS and HTML */
-        var articleCard =  `<div class="card text-center col-md-6 col-sm12 publicationCard" >
+        var articleCard =  `<div class="card text-center col-md-6 col-sm-12 publicationCard" >
                     <div class="card-body">
                         <h5 class="card-title">${arrayItem.title}</h5>
                         <p class="card-text">${arrayItem.author}</p>                        
